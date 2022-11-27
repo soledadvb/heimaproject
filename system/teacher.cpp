bf500de1c2156f9081cf23a18ff653cbbc16d06e
unknown <soledadvb>
2022-11-27 23:25:49 +0800

    first commit

---
+++ system/teacher.cpp
@@ -0,0 +1,21 @@
+#include "teacher.h"
+
+//默认构造
+Teacher::Teacher()
+{
+}
+
+//有参构造
+Teacher::Teacher(int empId, string name, string pwd)
+{
+}
+
+//菜单界面
+void Teacher :: operMenu()
+{
+}
+
+//查看所有预约
+void Teacher:: showAllOrder()
+{
+}
---
+++ system/teacher.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include "Identity.h"
+
+//教师类
+class Teacher :public Identity
+{
+public:
+	//默认构造
+	Teacher();
+
+	//有参构造
+	Teacher(int empId, string name, string pwd);
+
+	//菜单界面
+	virtual void operMenu();
+
+	//查看所有预约
+	void showAllOrder();
+
+	//审核预约
+	void validOrder();
+
+	//职工号
+	int m_EmpId;
+
+};
